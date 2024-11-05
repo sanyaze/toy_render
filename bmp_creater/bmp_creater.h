2fbe23cd040e119728294c7a17379bd7458538ba
sanyaze <sanyaze@outlook.com>
2024-11-05 23:52:49 +0800

    AR: #4 Description: add bmp creater Feature or Bugfix: Feature

---
+++ bmp_creater/bmp_creater.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iostream>
+
+#pragma pack(push, 1)
+struct BmpFileHeader
+{
+	uint16_t bfType = 0x4D42;
+    uint32_t bfSize;
+    uint16_t bfReserved1 = 0;
+    uint16_t bfReserved2 = 0;
+    uint32_t bfOffBits = 54;
+};
+
+struct BmpInfoHeader
+{
+	uint32_t biSize = 40;
+    int32_t  biWidth;
+    int32_t  biHeight;
+    uint16_t biPlanes = 1;
+    uint16_t biBitCount = 32; // 默认rgba 32位颜色
+    uint32_t biCompression = 0;
+    uint32_t biSizeImage = 0;
+    int32_t  biXPelsPerMeter = 2835;
+    int32_t  biYPelsPerMeter = 2835;
+    uint32_t biColorUsed = 0;
+    uint32_t biColorImportant = 0;
+};
+#pragma pack(pop)
+
+class BmpCreater
+{
+public:
+	BmpFileHeader fileHeader;
+    BmpInfoHeader infoHeader;
+    std::vector<uint32_t> data;
+    
+    BmpCreater(int width = 100, int height = 100)
+    {
+        infoHeader.biWidth = width;
+        infoHeader.biHeight = height;
+        data = std::vector<uint32_t>(width * height, 0xffffffff);
+        fileHeader.bfSize = sizeof(BmpFileHeader) + sizeof(BmpInfoHeader) + data.size() * sizeof(uint32_t);
+    }
+
+    void setPixel(int x, int y, uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0)
+    {
+        if (x < 0 || x >= infoHeader.biWidth || y < 0 || y >= infoHeader.biHeight) {
+            return;
+        }
+        int index = y * infoHeader.biWidth + x;
+        // 组装rgba为一个32位整数
+        uint32_t rgba = (a << 24) | (r << 16) | (g << 8) | b;
+        data[index] = rgba;
+    }
+
+    void save(const std::string path)
+    {
+        std::ofstream out(path.c_str(), std::ios_base::binary);
+        out.write((char*)&fileHeader, sizeof(fileHeader));
+        out.write((char*)&infoHeader, sizeof(infoHeader));
+        out.write((char*)data.data(), data.size() * sizeof(uint32_t));
+        out.close();
+    }
+};
--- main/main.cpp
+++ main/main.cpp
@@ -1,37 +1,22 @@
 #include "math_tools.h"
 #include "log_debug.h"
+#include "bmp_creater.h"
+#include <fstream>
 
 int main()
 {
-    Matrix3f m1, m2;
-    for (unsigned int i = 0; i < m1.row; i++) {
-        for (unsigned int j = 0; j < m1.col; j++) {
-            m1.data[i][j] = i + j;
-            m2.data[i][j] = i * j;
+    int width = 500;
+    int height = 500;
+    BmpCreater bmp(width, height);
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (i*i + j*j <= 90000) {
+                bmp.setPixel(i, j, 255, 0, 0);
+            }
         }
     }
     
-    Matrix3f m3 = Eye3();
-    show(m3);
-
-    Vector3f v1(1.0f, 2.0f, 3.0f);
-    Vector3f v2(1.0f, 1.0f, 1.0f);
-
-    show(m1);
-    show(m2);
-    show(v1);
-    show(v2);
-    
-    LOG_DEBUG("vector trans multiply:\n");
-    show(TransMultiply(v1, v2));
-    LOG_DEBUG("vector dot multiply:\n");
-    show(v1 * v2);
-    LOG_DEBUG("Mod: %f\n", v1.Mod());
-    
-    LOG_DEBUG("matrix multiply: \n");
-    show(m1 * m3);
-    LOG_DEBUG("mat * vec: \n");
-    show(m1 * v1);
-    LOG_DEBUG("matrix's det: %f\n", m1.Det());
+    bmp.save("test.bmp");
     return 0;
 }

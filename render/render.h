f5ac73271043cc0c08ed6672bae94a0ff1aa7a7c
sanyaze <sanyaze@outlook.com>
2024-12-05 00:53:27 +0800

    AR: #9 Description: 1. make render function              2. fix rotate bug Feature or Bugfix: Bugfix

--- render/render.h
+++ render/render.h
@@ -120,23 +120,91 @@ std::vector<Vector4d> RotatebyAxis(std::vector<Vector4d> v, Vector3d axis, doubl
     double ax = vAxis.data[0][0];
     double ay = vAxis.data[1][0];
     double az = vAxis.data[2][0];
-    
-    Matrix4d skew = Matrix4d();
-    skew.data[0][1] = -az;
-    skew.data[0][2] = ay;
-    skew.data[1][0] = az;
-    skew.data[1][2] = -ax;
-    skew.data[2][0] = -ay;
-    skew.data[2][1] = ax;
-    skew.data[3][3] = 1;
 
-    Matrix4d rot = Eye4() + sina * skew + k * (skew * skew);
+    /* 罗德里格斯矩阵, R(a, axis) = cos(a)I + (1 - cos(a))(axis * trans(axis)) + sin(a)[0, -az,  ay,
+     *                                                                                az,  0,  -ax,
+     *                                                                                -ay, ax,   0] */
+    Matrix4d rot = Eye4();
+    rot.data[0][0] = cosa + ax * ax * k;
+    rot.data[0][1] = ax * ay * k - az * sina;
+    rot.data[0][2] = ax * az * k + ay * sina;
+    rot.data[1][0] = ax * ay * k + az * sina;
+    rot.data[1][1] = cosa + ay * ay * k;
+    rot.data[1][2] = ay * az * k - ax;
+    rot.data[2][0] = ax * az * k - ay * sina;
+    rot.data[2][1] = ay * az * k + ax * sina;
+    rot.data[2][2] = cosa + az * az * k;
 
     std::vector<Vector4d> vRot(v.size(), Vector4d());
     for (int i = 0; i < v.size(); i++) {
         vRot[i] = rot * v[i];
     }
     return vRot;
+}
+
+// 渲染
+void Render(std::vector<Vector4d> vecCube, std::vector<std::vector<int>> face, std::vector<std::vector<int>> texture,
+            Matrix4d proj, int width, int height, BmpCreater &bmp)
+{
+    // 顶点投影
+    std::vector<Vector3d> vProj = VertexProj(vecCube, proj, width, height);
+    LOG_INFO("start render\n");
+
+    // 面元着色
+    std::vector<std::vector<double>> zBuffer(width, std::vector<double>(height, -INFINITY));
+    // 像素矩阵每行每列多两个点，方便后处理流程对边界的处理
+    std::vector<std::vector<double>> pixel_r(width + 2, std::vector<double>(height + 2, 0));
+    std::vector<std::vector<double>> pixel_g(width + 2, std::vector<double>(height + 2, 0));
+    std::vector<std::vector<double>> pixel_b(width + 2, std::vector<double>(height + 2, 0));
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            Vector2d v = Vector2d(double(j), double(i));
+            for (unsigned int k = 0; k < 6; k++) {
+                Vector3d t[2][3] = { { vProj[face[k][0]], vProj[face[k][1]], vProj[face[k][2]] },
+                                     { vProj[face[k][0]], vProj[face[k][2]], vProj[face[k][3]] } };
+                // 平面分为的两个三角面法向量一样，计算一次即可
+                Vector3d fNormal = Vector3d::Cross(t[0][1] - t[0][0], t[0][2] - t[0][0]);
+                for (int l = 0; l < 2; l++) {
+                    // 平面与z坐标轴平行，在XoY平面上的投影为一条直线, 不处理
+                    // 平面背对投影平面，即法向量朝向z轴反方向，不处理
+                    if (fNormal.data[2][0] < 0.00001) {
+                        continue;
+                    }
+                    if (IsPointInPlane(v, t[l])) {
+                        // 计算z轴坐标
+                        double vz = Vz(v, t[i], fNormal.Normal());
+                        if (vz > zBuffer[i][j]) {
+                            zBuffer[i][j] = vz;
+                            // 两侧的点为记录像素的边界，因此索引从1开始
+                            pixel_r[i+1][j+1] = texture[k][0];
+                            pixel_g[i+1][j+1] = texture[k][1];
+                            pixel_b[i+1][j+1] = texture[k][2];
+                        }
+                    }
+                }
+            }
+            int progress = (i * width + j) * 100 / (width * height);
+        }
+    }
+
+    // 后处理，抗锯齿
+    for (int i = 1; i <= height; i++) {
+        for (int j = 1; j <= width; j++) {
+            // 每9个像素合成一个像素点
+            int r = 0;
+            int g = 0;
+            int b = 0;
+            for (int pi = -1; pi <= 1; pi++) {
+                for (int pj = -1; pj <= 1; pj++) {
+                    r += pixel_r[i + pi][j + pj];
+                    g += pixel_g[i + pi][j + pj];
+                    b += pixel_b[i + pi][j + pj];
+                }
+            }
+            bmp.SetPixel(j - 1, i - 1, r / 9, g / 9, b / 9);
+            int progress = (i * width + j) * 100 / (width * height);
+        }
+    }
 }
 
 // 缩放变换
